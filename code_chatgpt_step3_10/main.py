36cf3fe07cc796f814a9abca8c253528421e0066
lswchina <1163418363@qq.com>
2023-09-22 17:11:14 +0800

    change default of parameters

--- code_chatgpt_step3_10/main.py
+++ code_chatgpt_step3_10/main.py
@@ -16,8 +16,8 @@ os.environ["https_proxy"] = "http://127.0.0.1:7890"
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", help = "input the name of an excel file in the dataset_2022 directory", dest = "excel_name", type = str, default = "benchmark2022.xlsx")
-    parser.add_argument("-l", help = "input the path to save logs", dest = "log_path", type = str, default = "../business/")
-    parser.add_argument("-o", help = "input the path to save results", dest = "res_path", type = str, default = "../result/")
+    parser.add_argument("-l", help = "input the path to save logs", dest = "log_path", type = str, default = "../../output/gpt4_vitas/")
+    parser.add_argument("-o", help = "input the path to save results", dest = "res_path", type = str, default = "./../output/gpt4_vitas/result/")
     args = parser.parse_args()
     EXCEL_PATH = "../dataset_2022/" + args.excel_name
     LOG_PATH = args.log_path
