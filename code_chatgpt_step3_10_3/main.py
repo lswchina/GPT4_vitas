81db7ddecff00a553d47a56a814eaa23af16a2e1
lswchina <1163418363@qq.com>
2023-09-26 17:50:54 +0800

    change from config9 to config10

--- code_chatgpt_step3_10_3/main.py
+++ code_chatgpt_step3_10_3/main.py
@@ -41,7 +41,7 @@ def init_dir(LOG_PATH, RESULT_PATH):
         os.makedirs(LOG_PATH)
 
 def init_constant():
-    Constant.CONFIG_PATH = '../config/config009.ini'
+    Constant.CONFIG_PATH = '../config/config010.ini'
     Constant.LOGOUT_URL = "https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1"
     Constant.CHROME_PATH = "../chrome/chromedriver_94"
     Constant.COOKIE_DIR = "../cookie/console_cookie7.pkl"
