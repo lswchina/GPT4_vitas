dd6da22ad6cb47ac4e92c1e4ea497737b820e5e2
lswchina <lisuwan@smail.nju.edu.cn>
2023-12-01 20:04:10 +0800

    prepare for large-scale testing

--- code_Gavin/main.py
+++ code_Gavin/main.py
@@ -12,24 +12,18 @@ import step2_test_skill as test
 
 def getArgs():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-i", help = "input the begin index", dest = "index", type=int, default=1)
     parser.add_argument("-c", help = "input the configuration file id", dest = "id", type=str, default="1")
-    parser.add_argument("-e", help = "input the name of an excel file in the dataset_2022 directory", dest = "excel_name", type = str, default = "benchmark2022.xlsx")
-    parser.add_argument("-l", help = "input the path to save logs", dest = "log_path", type = str, default = "../../output/gpt4_vitas/")
-    parser.add_argument("-o", help = "input the path to save results", dest = "res_path", type = str, default = "../../output/gpt4_vitas/result/")
+    parser.add_argument("-e", help = "input the name of an excel file in the dataset_2022 directory", dest = "excel_name", type = str, default = "2022_us_1.xlsx")
+    parser.add_argument("-l", help = "input the path to save logs", dest = "log_path", type = str, default = "../../output/gavin/")
     args = parser.parse_args()
+    INDEX = args.index
     CONFIG_ID = args.id
     EXCEL_PATH = "../dataset_2022/" + args.excel_name
     LOG_PATH = args.log_path
-    if LOG_PATH[-1] != '/':
-        LOG_PATH = LOG_PATH + '/'
-    RESULT_PATH = args.res_path
-    if RESULT_PATH[-1] != '/':
-        RESULT_PATH = RESULT_PATH + '/'
-    return CONFIG_ID, EXCEL_PATH, LOG_PATH, RESULT_PATH
+    return INDEX, CONFIG_ID, EXCEL_PATH, LOG_PATH
 
-def init_dir(LOG_PATH, RESULT_PATH):
-    if not os.path.exists(RESULT_PATH):
-        os.makedirs(RESULT_PATH)
+def init_dir(LOG_PATH):
     if not os.path.exists('../cookie'):
         os.makedirs("../cookie/")
     if not os.path.exists(LOG_PATH):
@@ -44,7 +38,7 @@ def init_constant(config_id):
         print("invalid configuration file id")
         sys.exit(-1)
     Constant.LOGOUT_URL = "https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1"
-    Constant.CHROME_PATH = "../chrome/chromedriver_94"
+    Constant.CHROME_PATH = "../chrome/chromedriver_103"
     Constant.COOKIE_DIR = "../cookie/console_cookie7_" + config_id + ".pkl"
     Constant.WEB = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
     Constant.USER_AGENT = {'User-Agent':Constant.WEB,'Host':'www.amazon.com'}
@@ -72,29 +66,34 @@ def init_constant(config_id):
     Constant.CONTEXT_RELATED_LABEL = "context-related"
 
 if __name__ == '__main__':
-    CONFIG_ID, EXCEL_PATH, LOG_PATH, RESULT_PATH = getArgs()
+    INDEX, CONFIG_ID, EXCEL_PATH, LOG_PATH = getArgs()
     if not os.path.exists(EXCEL_PATH):
         print("the excel path does not exist")
         sys.exit()
     init_constant(CONFIG_ID)
-    init_dir(LOG_PATH, RESULT_PATH)
+    init_dir(LOG_PATH)
     spider = Spider(Constant.CONFIG_PATH)
     UI.open_log_page(spider)
-    index = 1
     while True:
-        skill = Skill(EXCEL_PATH, index)
-        if skill.skillName == '<end_of_excel>' or index > 100:
+        skill = Skill(EXCEL_PATH, INDEX)
+        if skill.skillName == '<end_of_excel>':
             break
         if skill.skillName != '':
-            skill_log_path = os.path.join(LOG_PATH, re.sub(r'(\W+)', '_', skill.skillName))
+            skill_log_dir = os.path.join(LOG_PATH, re.sub(r'(\W+)', '_', skill.type))
+            if not os.path.exists(skill_log_dir):
+                os.makedirs(skill_log_dir)
+            result_path = os.path.join(skill_log_dir, "result")
+            if not os.path.exists(result_path):
+                os.makedirs(result_path)
+            skill_log_path = os.path.join(skill_log_dir, re.sub(r'(\W+)', '_', skill.skillName))
             if not os.path.exists(skill_log_path):
                 os.makedirs(skill_log_path)
             else:
-                index = index + 1
+                INDEX = INDEX + 1
                 continue
             gpt = askChatGPT(skill.skillName, skill_log_path, True, Constant.CONFIG_PATH)
             fsm = FSM(gpt)
-            test.generateTest(skill_log_path, RESULT_PATH, spider, skill, gpt, fsm)
+            test.generateTest(skill_log_path, result_path, spider, skill, gpt, fsm)
             UI.re_open_with_no_exit(spider)
-        index = index + 1
+        INDEX = INDEX + 1
     UI.close_spider(spider)
