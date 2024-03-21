21288816097d5f231849ebd9fe80186486a5e5c1
lswchina <lisuwan@smail.nju.edu.cn>
2024-03-21 14:14:30 +0800

    delete cookie

--- code_Gavin_10/main.py
+++ code_Gavin_10/main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     else:
         model = None
         tokenizer = None
-    index = 1
+    index = 2
     while True:
         skill = Skill(EXCEL_PATH, LOG_PATH, index)
         if skill.skillName == '<end_of_excel>':# or index > 40:
