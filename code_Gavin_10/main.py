96e2328970ae7426aa248f1cf8871000330a046a
lswchina <lisuwan@smail.nju.edu.cn>
2024-09-29 11:30:11 +0800

    change index

--- code_Gavin_10/main.py
+++ code_Gavin_10/main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     else:
         model = None
         tokenizer = None
-    index = 24
+    index = 30
     while True:
         skill = Skill(EXCEL_PATH, LOG_PATH, index)
         if skill.skillName == '<end_of_excel>':
