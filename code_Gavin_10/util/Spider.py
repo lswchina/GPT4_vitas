ea38ed81fd01eb51ad81cf399ce9e2efc798ad60
lswchina <lisuwan@smail.nju.edu.cn>
2025-02-06 16:33:25 +0800

    change config

--- code_Gavin_10/util/Spider.py
+++ code_Gavin_10/util/Spider.py
@@ -20,9 +20,9 @@ from distutils.version import StrictVersion
 class Spider:
     def __init__(self, config_path: str):
         chrome_options = Options()
-        '''chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--headless')'''
+        chrome_options.add_argument('--headless')
         if StrictVersion(selenium.__version__) < StrictVersion("4.0.0"):
             self.web_driver = webdriver.Chrome(options= chrome_options) #, executable_path=Constant.CHROME_PATH)
         else:
